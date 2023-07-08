34ea21d1d5fcabcba76bff1a828adb73ff7cd2d4
Matheus Pereira <matheusgp.mto@outlook.com>
2023-07-08 00:09:35 -0300

    Criando a rota de atualização dos endereços do usuário

--- Express - API/src/controllers/addresses.controller.js
+++ Express - API/src/controllers/addresses.controller.js
@@ -1,18 +1,58 @@
 import addresses from '../models/addresses.model.js';
+import clients from '../models/clients.model.js';
 import ClientsController from '../controllers/clients.controller.js';
 
 class AddressesController {
+
+	static async listAddresses(req, res, next) {
+		try {
+			const addressesList = await addresses
+				.find()
+				.select({
+					_id: 0, id: 0,
+					createdAt: 0,
+					updatedAt: 0
+				})
+			;
+
+			res.status(200).json(addressesList);
+		}catch(error) {
+			next(error);
+		}
+	}
 
 	static async createAddress(req, res, next) {
 		try {
 			const address = new addresses(req.body);
 
-			req.body = { addressId: address._id };
+			req.body = { address: address._id };
 
 			await address.save();
 			ClientsController.updateCustomer(req, res, next);
 
 			res.status(201).json({ message: 'Address registered successfully!' });
+		}catch(error) {
+			next(error);
+		}
+	}
+
+	static async updateAddress(req, res, next) {
+		let client;
+		let addressId;
+
+		try {
+			const { cpf, email } = req.headers;
+
+			if(cpf) {
+				client = await clients.findOne({ cpf: cpf });
+			}else if(email) {
+				client = await clients.findOne({ email: email });
+			}
+
+			addressId = client.address._id;
+			await addresses.findByIdAndUpdate(addressId, req.body, { new: true });
+			
+			res.status(201).json({ message: 'Address updated successfully!' });
 		}catch(error) {
 			next(error);
 		}
--- Express - API/src/routes/addresses.route.js
+++ Express - API/src/routes/addresses.route.js
@@ -4,6 +4,8 @@ import AddressesController from '../controllers/addresses.controller.js';
 const router = express.Router();
 
 router
+	.get('/addresses', AddressesController.listAddresses)
 	.post('/addresses', AddressesController.createAddress)
+	.put('/addresses', AddressesController.updateAddress)
 
 export default router;
